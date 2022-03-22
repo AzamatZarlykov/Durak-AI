0e032479098df672d957ff044bb73cc5119e8023
Azamat Zarlykov <azarlykovcz@gmail.com>
2022-03-22 23:59:19 +0100

    added view reference to CLI

--- Durak-AI/CLI/Program.cs
+++ Durak-AI/CLI/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 
+using View;
+
 namespace CLI
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            View.GameUI.StartWindow();
+
+            // CLI: 2 AI to play each other and configure their
         }
     }
 }
+
+// dotnet run "ai_1_name" [options for ai_1_name] "ai_2_name" [options for ai_2_name] 
---
+++ Durak-AI/View/GameUI.cs
@@ -0,0 +1,58 @@
+﻿using Cairo;
+using Gdk;
+using Gtk;
+using System;
+
+namespace View
+{
+    class Area : DrawingArea
+    {
+        bool draw = true;           // model
+
+        public Area()
+        {
+            AddEvents((int)EventMask.ButtonPressMask);
+        }
+
+        protected override bool OnButtonPressEvent(EventButton e)
+        {
+            draw = !draw;
+            QueueDraw();
+            return true;
+        }
+
+        protected override bool OnDrawn(Context c)
+        {
+            if (draw)
+            {
+                c.SetSourceRGB(0.5, 0.5, 0.0);  // olive color
+                c.Arc(250, 250, 150, 0.0, 2 * Math.PI);
+                c.Fill();
+            }
+            return true;
+        }
+    }
+
+    public class GameUI : Gtk.Window
+    {
+        public GameUI() : base("circle")
+        {
+            Resize(500, 500);
+            Add(new Area());
+        }
+
+        protected override bool OnDeleteEvent(Event ev)
+        {
+            Application.Quit();
+            return true;
+        }
+
+        public static void StartWindow()
+        {
+            Application.Init();
+            GameUI w = new GameUI();
+            w.ShowAll();
+            Application.Run();
+        }
+    }
+}
